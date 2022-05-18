7295fb5851241924de5bcd7ffd8a3cc7b53ae3c8
Brooke <brookepaglia@gmail.com>
2022-05-18 17:14:22 -0400

    add typeDef for Queries and Mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,16 @@
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
 const resolvers = {
-  Query: {},
+  Query: {
+    users: async () => {
+      return User.find().select("-__v -password");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).select("-__v -password");
+    },
+  },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -17,6 +17,22 @@ const typeDefs = gql`
     email: String
     savedBooks: [Book]
   }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!) Auth
+    saveBook(user: ID!, authors: [String], description: String!, bookId: String!, image: String, link: String, title: String!)
+  }
 `;
 
 module.exports = typeDefs;
